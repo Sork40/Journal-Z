0c653474bfd8cd3020790690d218abca108409a6
Sork40 <19tb91@gmail.com>
2025-07-20 17:28:30 +0200

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,11 +1,14 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  $('#flipbook').turn({
-    width: 922,
-    height: 600,
-    autoCenter: true,
-    gradients: true,
-    elevation: 50,
-    duration: 1500
-  });
+window.addEventListener("load", function () {
+  if (window.jQuery && typeof $('#flipbook').turn === 'function') {
+    $('#flipbook').turn({
+      width: 922,
+      height: 600,
+      autoCenter: true,
+      gradients: true,
+      elevation: 50,
+      duration: 1500
+    });
+  } else {
+    console.error("jQuery ou turn.js n'est pas chargé correctement.");
+  }
 });

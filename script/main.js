72ba67627266b920fc259046eb0441736fca3433
Sork40 <19tb91@gmail.com>
2025-07-22 00:53:05 +0200

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,28 +1,23 @@
 
-window.addEventListener("load", function () {
-  const flipbook = document.getElementById("flipbook");
-  if (window.jQuery && typeof $('#flipbook').turn === 'function') {
-    $('#flipbook').turn({
-      width: window.innerWidth,
-      height: window.innerHeight,
-      autoCenter: true,
-      elevation: 50,
-      duration: 1200,
-      gradients: true
-    });
+$(document).ready(function () {
+  $('#flipbook').turn({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    autoCenter: true,
+    elevation: 50,
+    duration: 1000,
+    gradients: true
+  });
 
-    document.getElementById("prev").addEventListener("click", () => {
-      $('#flipbook').turn('previous');
-    });
+  $('#prev').click(function () {
+    $('#flipbook').turn('previous');
+  });
 
-    document.getElementById("next").addEventListener("click", () => {
-      $('#flipbook').turn('next');
-    });
+  $('#next').click(function () {
+    $('#flipbook').turn('next');
+  });
 
-    window.addEventListener("resize", () => {
-      $('#flipbook').turn("size", window.innerWidth, window.innerHeight);
-    });
-  } else {
-    console.error("jQuery ou turn.js n'est pas chargé correctement.");
-  }
+  $(window).on('resize', function () {
+    $('#flipbook').turn('size', window.innerWidth, window.innerHeight);
+  });
 });

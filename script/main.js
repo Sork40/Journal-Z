d214df47a4cb923eb90bcb16593991f4cbe226ff
Sork40 <19tb91@gmail.com>
2025-07-22 00:45:43 +0200

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,12 +1,26 @@
+
 window.addEventListener("load", function () {
+  const flipbook = document.getElementById("flipbook");
   if (window.jQuery && typeof $('#flipbook').turn === 'function') {
     $('#flipbook').turn({
-      width: 922,
-      height: 600,
+      width: window.innerWidth,
+      height: window.innerHeight,
       autoCenter: true,
-      gradients: true,
       elevation: 50,
-      duration: 1500
+      duration: 1200,
+      gradients: true
+    });
+
+    document.getElementById("prev").addEventListener("click", () => {
+      $('#flipbook').turn('previous');
+    });
+
+    document.getElementById("next").addEventListener("click", () => {
+      $('#flipbook').turn('next');
+    });
+
+    window.addEventListener("resize", () => {
+      $('#flipbook').turn("size", window.innerWidth, window.innerHeight);
     });
   } else {
     console.error("jQuery ou turn.js n'est pas chargé correctement.");
